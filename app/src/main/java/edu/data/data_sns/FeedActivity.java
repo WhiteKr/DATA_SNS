2b152bf41d22163a334a36e586cdea88ccd8d68a
WhiteKr <hanjisu2215g@gmail.com>
2020-08-20 18:23:04 +0900

    Added Sign Up Button on FeedActivity.java

--- app/src/main/java/edu/data/data_sns/FeedActivity.java
+++ app/src/main/java/edu/data/data_sns/FeedActivity.java
@@ -2,7 +2,11 @@ package edu.data.data_sns;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class FeedActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class FeedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+
+        Button signUp2Btn = findViewById(R.id.signUp2_btn);
+        signUp2Btn.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View view) {
+                        Log.d("DATA_SNS", "Sign Up Button Clicked!!! (in FeedActivity)");
+
+                        Intent intent = new Intent(FeedActivity.this, SignUpActivity.class);
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 }
