b289ab0865ca406dc475a97b42ed4e117d053a72
WhiteKr <hanjisu2215g@gmail.com>
2020-08-20 17:26:01 +0900

    Added new Activities and Layouts for Log In and Sign Up.

--- app/src/main/java/edu/data/data_sns/MainActivity.java
+++ app/src/main/java/edu/data/data_sns/MainActivity.java
@@ -1,7 +1,12 @@
 package edu.data.data_sns;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginBtn = findViewById(R.id.login_btn);
+        loginBtn.setOnClickListener(
+            new Button.OnClickListener() {
+                public void onClick(View view) {
+                    Log.d("Data_SNS", "Login Button Clicked!!!");
+
+                    Intent intent = new Intent(MainActivity.this, FeedActivity.class);
+                    startActivity(intent);
+                }
+            }
+        );
+
+        Button signUpBtn = findViewById(R.id.signUp_btn);
+        signUpBtn.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View view) {
+                        Log.d("DATA_SNS", "Sign Up Button Clicked!!!");
+
+                        Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 }
